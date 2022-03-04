6d57bded2818e6876624b684b6fe79cfb72eb32a
sriam <sriaman3@gmail.com>
2022-03-04 07:01:16 +0530

    this is my subtraction progrma

---
+++ src/com/gitTesting/SybtractionProgram.java
@@ -0,0 +1,14 @@
+package com.gitTesting;
+
+public class SybtractionProgram {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b=20;
+		
+		int c=b-a;
+		//This will give you output
+		 System.out.println(c);
+	}
+
+}
